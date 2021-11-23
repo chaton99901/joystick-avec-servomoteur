e7fb1c873bf4adea3d1b4bf8f1dc8ee9d2d11789
chaton99901 <94067162+chaton99901@users.noreply.github.com>
2021-11-23 12:56:08 -0500

    Mettre à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,10 @@
+servos.P0.setRange(10, 170)
 basic.forever(function () {
-    led.plot(pins.map(
+    pins.servoWritePin(AnalogPin.P0, pins.map(
     pins.analogReadPin(AnalogPin.P1),
     0,
     1023,
-    0,
-    4
-    ), pins.map(
-    pins.analogReadPin(AnalogPin.P2),
-    0,
-    1023,
-    0,
-    4
+    10,
+    170
     ))
 })
